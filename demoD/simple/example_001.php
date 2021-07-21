98a6c07d75d66cd0d14e517ae77ecfe924bead33
kcpic <85400908+kcpic@users.noreply.github.com>
2021-07-21 21:34:24 +0700

    Update example_001.php
    fix?

--- demoD/simple/example_001.php
+++ demoD/simple/example_001.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test TCPDF PHP file</title>
+</head>
+<body>
+
 <?php
 //============================================================+
 // File name   : example_001.php
@@ -104,3 +114,10 @@ $pdf->Output('example_001.pdf', 'I');
 //============================================================+
 // END OF FILE
 //============================================================+
+?>   
+    </body>
+        </html>
+    
+    
+ 
+    
